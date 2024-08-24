6091fce110839526c0251f4b25c1afa0ce7124e2
Jimmy Reichley <jimmyqpublik@gmail.com>
2024-08-24 11:48:00 -0400

    Initial

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Bato-nix";
+
+  inputs = {
+  nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = import ./shell.nix { inherit pkgs; };
+      }
+    );
+}
