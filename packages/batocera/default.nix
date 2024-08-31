79d7bd49697cce739a654b572826e4ec47a328e3
Jimmy Reichley <jimmyqpublik@gmail.com>
2024-08-30 21:15:25 -0400

    Initial emulatorlauncher work

--- packages/batocera/default.nix
+++ packages/batocera/default.nix
@@ -1,8 +1,10 @@
 { lib
 , writeTextFile
 , stdenv
+, makeWrapper
+, python311Packages
 
-, python3
+, python311
 , rsync
 
 , batocera-src
@@ -10,11 +12,34 @@
 }:
 
 let
+  python3 = python311;
+  python3Packages = python311Packages;
+
   config-file = writeTextFile {
     name = "bato-config";
     text = ''
       ${lib.concatMapStrings (emu: "${emu}=y\n") emus}
     '';
+  };
+
+  emulator-launcher = python3Packages.buildPythonApplication rec {
+    pname = "emulator-launcher";
+    version = "2024-08-30";
+
+    sourceRoot = "source/package/batocera/core/batocera-configgen/configgen";
+    src = batocera-src;
+
+    patchPhase = ''
+      runHook prePatch
+
+      find ./ -type f -exec sed -i -e 's|/usr/share/|/run/current-system/sw/share/|g' {} \;
+
+      runHook postPatch
+    '';
+
+    build-system = [
+      python3Packages.setuptools
+    ];
   };
 in
 stdenv.mkDerivation (finalAttrs: {
@@ -23,6 +48,15 @@ stdenv.mkDerivation (finalAttrs: {
   version = "v39";
 
   src = batocera-src;
+  patchPhase = ''
+    runHook prePatch
+
+    pushd package/batocera/core/batocera-configgen/configgen
+    find . -type f -exec sed -i -e 's|/usr/share/|/run/current-system/sw/share/|g' {} \;
+    popd
+
+    runHook postPatch
+  '';
 
   strictDeps = true;
 
@@ -30,45 +64,68 @@ stdenv.mkDerivation (finalAttrs: {
     rsync
     python3
     python3.pkgs.pyyaml
+    makeWrapper
+  ];
+
+  buildInputs = [
+    python3
+    # python3.pkgs.pyyaml
+    python3.pkgs.pyudev
+    python3.pkgs.evdev
+    python3.pkgs.pillow
+    emulator-launcher
   ];
 
   buildPhase = ''
     runHook preBuild
 
+    currentSrc=$(pwd)
+
     mkdir -p $out/tmp
-    rsync -a $src/package/batocera/emulationstation/batocera-es-system/roms $out/tmp/
+    rsync -a package/batocera/emulationstation/batocera-es-system/roms $out/tmp/
     chmod -R +rw $out/tmp/roms
 
     mkdir -p $out/result
-    cd $src/package/batocera/emulationstation/batocera-es-system
+    pushd package/batocera/emulationstation/batocera-es-system
     python3 batocera-es-system.py \
-      $src/package/batocera/emulationstation/batocera-es-system/es_systems.yml \
-      $src/package/batocera/emulationstation/batocera-es-system/es_features.yml \
+      $currentSrc/package/batocera/emulationstation/batocera-es-system/es_systems.yml \
+      $currentSrc/package/batocera/emulationstation/batocera-es-system/es_features.yml \
       $out/result/es_external_translations.h \
       $out/result/es_keys_translations.h \
-      $src/package/batocera \
-      $src/package/batocera/emulationstation/batocera-es-system/locales/blacklisted-words.txt \
+      $currentSrc/package/batocera \
+      $currentSrc/package/batocera/emulationstation/batocera-es-system/locales/blacklisted-words.txt \
       ${config-file} \
       $out/result/es_systems.cfg \
       $out/result/es_features.cfg \
-      $src/package/batocera/core/batocera-configgen/configs/configgen-defaults.yml \
-      $src/package/batocera/core/batocera-configgen/configs/configgen-defaults-x86_64.yml \
+      $currentSrc/package/batocera/core/batocera-configgen/configs/configgen-defaults.yml \
+      $currentSrc/package/batocera/core/batocera-configgen/configs/configgen-defaults-x86_64.yml \
       $out/tmp/roms \
       $out/result/roms \
       x86_64
+    popd
 
     mkdir -p $out/datainit/system/configs/emulationstation
-    cp $src/package/batocera/core/batocera-system/batocera.conf $out/datainit/system/
-    cp $src/package/batocera/core/batocera-system/batocera-boot.conf $out/datainit/
-    cp $src/package/batocera/emulationstation/batocera-emulationstation/controllers/es_input.cfg $out/datainit/system/configs/emulationstation/
+    cp $currentSrc/package/batocera/core/batocera-system/batocera.conf $out/datainit/system/
+    cp $currentSrc/package/batocera/core/batocera-system/batocera-boot.conf $out/datainit/
+    cp $currentSrc/package/batocera/emulationstation/batocera-emulationstation/controllers/es_input.cfg $out/datainit/system/configs/emulationstation/
 
     mkdir -p $out/init.d
-    cp $src/board/batocera/fsoverlay/etc/init.d/rcS $out/init.d/
+    cp $currentSrc/board/batocera/fsoverlay/etc/init.d/rcS $out/init.d/
     substituteInPlace $out/init.d/rcS \
       --replace-fail "for i in /etc/init.d/" "for i in $out/init.d/"
-    cp $src/board/batocera/fsoverlay/etc/init.d/S12populateshare $out/init.d/
+    cp $currentSrc/board/batocera/fsoverlay/etc/init.d/S12populateshare $out/init.d/
     substituteInPlace $out/init.d/S12populateshare \
       --replace-fail "IN=/usr/share/batocera/datainit" "IN=$out/datainit"
+
+    mkdir -p $out/{bin,opt}
+    rsync -a $currentSrc/package/batocera/core/batocera-configgen $out/opt/
+
+    ln -sf $out/opt/batocera-configgen/configgen/configgen/emulatorlauncher.py $out/bin/emulatorlauncher
+    chmod +x $out/bin/emulatorlauncher
+
+    wrapProgram $out/bin/emulatorlauncher \
+      --prefix PATH : ${lib.makeBinPath [ python3 ]} \
+      --prefix PYTHONPATH : "$PYTHONPATH"
 
     rm -rf $out/tmp
 
@@ -78,12 +135,20 @@ stdenv.mkDerivation (finalAttrs: {
   installPhase = ''
     runHook preInstall
 
+    currentSrc=$(pwd)
+
     mkdir -p $out/share/emulationstation
     # TODO(jpr): move these to ES src folder
     mv $out/result/es_external_translations.h $out/share/es_external_translations.h
     mv $out/result/es_keys_translations.h $out/share/es_keys_translations.h
     install -Dm 0644 $out/result/es_systems.cfg $out/share/emulationstation/es_systems.cfg
     install -Dm 0644 $out/result/es_features.cfg $out/share/emulationstation/es_features.cfg
+
+    mkdir -p $out/share/batocera/configgen
+    cp $currentSrc/package/batocera/core/batocera-configgen/configs/configgen-defaults.yml \
+      $out/share/batocera/configgen/configgen-defaults.yml
+    cp $currentSrc/package/batocera/core/batocera-configgen/configs/configgen-defaults-x86_64.yml \
+      $out/share/batocera/configgen/configgen-defaults-arch.yml
 
     mkdir -p $out/opt
     mv $out/result/roms $out/opt
